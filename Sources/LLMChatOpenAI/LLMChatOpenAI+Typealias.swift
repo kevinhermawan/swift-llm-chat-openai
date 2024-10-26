f964c6d5982a750ab90df0f2b08abb55b0f00777
Kevin Hermawan <84965338+kevinhermawan@users.noreply.github.com>
2024-10-27 04:12:44 +0700

    feat: adds type aliases (#11)

---
+++ Sources/LLMChatOpenAI/LLMChatOpenAI+Typealias.swift
@@ -0,0 +1,15 @@
+//
+//  LLMChatOpenAI+Typealias.swift
+//  LLMChatOpenAI
+//
+//  Created by Kevin Hermawan on 10/27/24.
+//
+
+import Foundation
+
+public extension LLMChatOpenAI {
+    typealias Completion = ChatCompletion
+    typealias CompletionChunk = ChatCompletionChunk
+    typealias Message = ChatMessage
+    typealias Options = ChatOptions
+}
